41d3eded00dad329b09d2a026a41c91443330f38
jim <contactojaimecz@gmail.com>
2023-03-29 18:35:13 -0700

    cdir public subnets

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -8,7 +8,9 @@ resource "aws_vpc" "vpc_api" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.vpc_api.id
-  cidr_block              = var.public_subnets_cidr
+  count                   = length(var.public_subnets_cidr)
+  cidr_block              = element(var.public_subnets_cidr,count.index)
+  availability_zone       = element(var.availability_zone, count.index)
   map_public_ip_on_launch = true
   tags = {
     Name        = "public-subnet"
