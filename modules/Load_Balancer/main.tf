60de46eb09124ffac6a05885c0ccca39f75e38df
jim <contactojaimecz@gmail.com>
2023-03-29 19:24:44 -0700

    ALB

--- modules/Load_Balancer/main.tf
+++ modules/Load_Balancer/main.tf
@@ -1,21 +1,34 @@
-resource "aws_security_group" "lb_sg" {
-  name_prefix = "lb_sg"
+resource "aws_elb" "ALB" {
+  name            = "ALB-elb"
+  subnets         = [aws_subnet.public.*.id]
+  security_groups = [aws_security_group.elb.id]
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  listener {
+    instance_port     = 80
+    instance_protocol = "HTTP"
+    lb_port           = 80
+    lb_protocol       = "HTTP"
   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  listener {
+    instance_port      = 80
+    instance_protocol  = "HTTP"
+    lb_port            = 443
+    lb_protocol        = "HTTPS"
+    ssl_certificate_id = aws_acm_certificate.ALB.arn
   }
 
-  tags = {
-    Name = "lb_sg"
+  # Define an Elastic IP to associate with the ELB
+  resource "aws_eip" "ALB" {
+    vpc = true
   }
+
+  # Associate the Elastic IP with the ELB
+  resource "aws_elb_attachment" "ALB" {
+    elb_id     = aws_elb.ALB.id
+    instance_id = aws_instance.ALB.id
+    attach_type = "in"
+  }
+
+  depends_on = [aws_acm_certificate.ALB]
 }
